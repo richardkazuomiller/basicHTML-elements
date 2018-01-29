b95cfee6a0a85ccecfa09cae891cb807842e0f4d
Ricky Miller <ricky@ricky.me>
2018-01-29 14:39:50 +0900

    Implement input.value

--- test/lib/elements/HTMLInputElement.js
+++ test/lib/elements/HTMLInputElement.js
@@ -8,4 +8,16 @@ describe('HTMLInputElement',() => {
       assert.equal(input.getAttribute('name'),'foobar');
     });
   });
+  describe('value',() => {
+    it('should update the attribute',() => {
+      const input = document.createElement('input');
+      input.value = 'hoge';
+      assert.equal(input.getAttribute('value'),'hoge');
+    });
+    it('should reflect the attribute',() => {
+      const input = document.createElement('input');
+      input.setAttribute('value','piyo');
+      assert.equal(input.value,'piyo');
+    });
+  })
 });
